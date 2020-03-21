0683cac3a88b722bc6c4907d1534c67737ef6a23
xun <1551885@tongji.edu.cn>
2020-03-21 11:56:43 +0800

    update some problems.

--- uva/UVA12096 The SetStack Computer.cpp
+++ uva/UVA12096 The SetStack Computer.cpp
@@ -5,6 +5,27 @@
 #include <set>
 using namespace std;
 
+/*
+ * 这个题兜兜转转终于通过了！一开始做的时候完全没有思路，想的是
+ * 用 string 来表示每一个 set of set，但是这样做的开销非常大，
+ * 因为这里的 string 可能会很长很长。
+ *
+ * 后来的想法是对于集合里的每一个元素都用 int 来代表，栈式计算机
+ * 存在五种基本操作，其中只有 ADD 操作会产生新的元素，此时需要
+ * 查看是否已经为集合 A 分配过了 id，否则则为它分配新的 id，随后
+ * 将其 id 插入到集合 B 中即可。
+ *
+ * 我这里的实现还是比较愚蠢的，比如说仍然是将一个集合转换成string
+ * 以后再与 id 建立映射的，实际上直接用 set<int> 也是可以作为 map
+ * 的键值（只是我当时不知道）
+ *
+ * 此前的代码还存在一个bug，是在 Intersection 操作中，我的实现
+ * 依次取出集合 A 的每一个元素，判断它是否在 B 中存在，否则将
+ * 该元素在集合 A 中删除。由于在 foreach 遍历中删除元素，会产生
+ * 一些奇怪的问题，正确的实现应该是将两个集合都存在的元素插入到
+ * 一个新的集合当中。
+ */
+
 stack<set<int>> s;
 map<string, int> m;
 
@@ -21,7 +42,7 @@ int main(){
     int T, N, cnt, curr;
     char input[10];
     string hash;
-    set<int> tmp1, tmp2;
+    set<int> tmp1, tmp2, tmp;
     scanf("%d", &T);
     while(T--){
         scanf("%d", &N);
@@ -45,10 +66,11 @@ int main(){
                 }
                 else
                 if(input[0] == 'I'){
+                    tmp.clear();
                     for(int e : tmp1)
-                        if(tmp2.find(e) == tmp2.end())
-                            tmp1.erase(e);
-                    s.push(tmp1);
+                        if(tmp2.find(e) != tmp2.end())
+                            tmp.insert(e);
+                    s.push(tmp);
                 }
                 else{
                     hash = tostr(tmp1);
--- uva/UVA1592 Database.cpp
+++ uva/UVA1592 Database.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 using namespace std;
 
+/*
+ * 这个题没想到也是颇费了一番周折，本来的代码就是直接暴力枚举，
+ * （因为我也想不出其他更优的方法了......），意料之中地超时了。
+ *
+ * 更优的算法是对列进行枚举，对于每两列，再枚举所有的行，将这
+ * 两列的值存储到一个 map 当中。如果在某一行发现当前值已经在
+ * map 中出现过，就说明不满足 PNF 范式。该算法的时间复杂度为
+ * O(m^2 x nlogn)，比我的 O(n^2 x m) 好了不少。
+ *
+ * 上面的算法还可以进行进一步的优化，比如将一开始输入的所有串
+ * 都赋予一个唯一的编号，在表中只用存储这些编号即可，就可以避免
+ * 后续 map 中操作字符串比较带来的开销。
+ */
+
 vector<vector<string>> database;
+map<string, int> entry;
 
 void parse(const string &str, int rownum){
 	int begin = 0, next;
@@ -20,6 +36,7 @@ int main(){
 	freopen("1.out", "w", stdout);
 	int m, n, cnt;
 	int row1, row2, col1, col2;
+	bool flag;
 	string str;
 	while(cin >> n >> m){
 		cin.ignore();
@@ -33,31 +50,27 @@ int main(){
 				cout << s << ' ';
 			cout << endl;*/
 		}
-		for(int ix = 0; ix < n; ++ix)
-			for(int jx = ix + 1; jx < n; ++jx){
-				cnt = 0;
-				for(int kx = 0; kx != m; ++kx)
-					if(database[ix][kx] == database[jx][kx])
-						++cnt;
-				if(cnt >= 2){
-					row1 = ix, row2 = jx;
-					goto out;
+		for(int ix = 0; ix < m; ++ix)
+			for(int jx = ix + 1; jx < m; ++jx){
+				entry.clear();
+				flag = false;
+				for(int kx = 0; kx != n; ++kx){
+					str = database[kx][ix] + ',' + database[kx][jx];
+					if(entry.find(str) == entry.end())
+						entry[str] = kx;
+					else{
+						row1 = entry[str], row2 = kx;
+						col1 = ix, col2 = jx;
+						flag = true;
+						goto out;
+					}
 				}
 			}
 		out:
-		if(cnt < 2) printf("YES\n");
-		else{
-			cnt = 0;
-			printf("NO\n");
-			printf("%d %d\n", row1 + 1, row2 + 1);
-			for(int ix = 0; ix != m; ++ix)
-				if(database[row1][ix] == database[row2][ix]){
-					++cnt;
-					if(cnt == 1) col1 = ix;
-					if(cnt == 2) col2 = ix;
-				}
-			printf("%d %d\n", col1 + 1, col2 + 1);
-		}	
+		if(flag)
+			printf("NO\n%d %d\n%d %d\n", row1 + 1, row2 + 1, col1 + 1, col2 + 1);
+		else
+			printf("YES\n");
 	}
 	return 0;
 }
